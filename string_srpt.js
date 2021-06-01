1f5ec06cdfa6cbd40ca8550099439bdde01c9532
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-01 23:25:18 +0530

    String and Number Final

--- string_srpt.js
+++ string_srpt.js
@@ -1,2 +1,35 @@
 //string practice
 console.log("STRING PRACTICE");
+
+//String
+const singleQuotes = 'hello!';
+const doubleQuotes = "hello!!";
+const backticks = `after this , ${doubleQuotes} , hello!!!`;
+
+//  use it when needed extented functionality
+const backticks_2 = `${2+2}`;
+
+console.log(singleQuotes);
+// console.log(doubleQuotes);
+console.log(backticks);
+console.log(backticks_2);
+
+//Numbers practices
+console.log("NUMBER PRACTICE");
+
+const wholeNumber = 77;
+const decimalNumber = 0.77;
+
+const firstNumber = 10;
+const secondNumber = 5;
+const result = firstNumber + secondNumber ;
+const result2 = `${firstNumber + secondNumber}` ;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+// first result output is number 
+console.log(result);
+console.log(typeof result);
+// result2 output is string
+console.log(result2);
+console.log(typeof result2);
