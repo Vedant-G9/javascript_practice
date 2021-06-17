20770c8148ada7cf99180e27db1fb8722cd298c0
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-17 14:09:43 +0530

    comparison operator

---
+++ operators/comparison_script.js
@@ -0,0 +1,48 @@
+console.log("Comparison operators");
+
+const a = 10;
+const b = 10;
+
+// < , <= , > , >= , = 
+//comaprisoon operator always give Boolean Value
+
+//greater than
+console.log(a > b);
+
+//greater than equal to
+//console.log(a >= b);
+
+//less than
+// console.log(a < b);
+
+//less than equal to
+// console.log(a <= b);
+
+//single '=' for assign
+//double '==' to compare
+
+//is equals
+//console.log(a == b);
+
+//is not equals
+//console.log(a != b);
+
+//strictly equals
+//console.log(a === b);
+
+//strictly not equals
+//console.log(a !== b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
