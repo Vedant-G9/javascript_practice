bbe87d2dcfe25df4db7941973eec3a8c7cc5a297
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-18 18:02:06 +0530

    logical operator

---
+++ operators/logical_oprt.js
@@ -0,0 +1,14 @@
+console.log("Logical_operator");
+
+//logical operator
+// AND=>&&  both have to be same
+console.log(true && true);
+
+// AND=>&&  all have to be same
+console.log(true && true && true);
+
+// OR=> || any have to be true
+console.log(false || true);
+
+// NOT=> ! to not
+console.log(!false);
