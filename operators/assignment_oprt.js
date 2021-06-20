7d3384ffb420064f8a3a6dab19efe0daf90489f1
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-20 14:27:00 +0530

    Assignment Operator

---
+++ operators/assignment_oprt.js
@@ -0,0 +1,16 @@
+console.log("Assignment operator");
+
+let number = 9;
+
+ number += 9; //number = number + 9; <--same
+// number -= 9; //number = number - 9; <--same
+// number *= 9; //number = number * 9; <--same
+// number /= 9; //number = number / 9; <--same
+
+console.log(number);
+
+let string = 'Hello';
+
+    string += ' World !!';
+
+    console.log(string);
