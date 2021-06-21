fabff7670af17081a71d9314454ec14938c6dca3
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-21 12:19:28 +0530

    control_flow_if_else_statements

---
+++ logica and control flow/script.js
@@ -0,0 +1,19 @@
+console.log("Logical and Control flow"); 
+
+const age = 18;
+
+/**
+         * if (condition){
+         *      block of code
+         * } else {
+         *      block of code
+         * }
+ * 
+ */
+if(age > 18){
+    console.log("welcome , you can enter !");
+} else if (age === 18){
+    console.log("you'er just truned 18 welcome !");
+} else {
+    console.log("Grow up child !");
+}
