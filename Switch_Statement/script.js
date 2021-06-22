5051b8d7b1fd7076d72056dc7b63d43f812f9469
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-23 00:06:32 +0530

    switch_statement

---
+++ Switch_Statement/script.js
@@ -0,0 +1,24 @@
+console.log("Switch_Statement");
+
+const avenger = 'Bat Man';
+switch(avenger){
+    case 'captain America':
+        console.log("i am Stev Rogers !");
+        break;
+    case 'Hulk':
+        console.log("smash !");
+        break;
+    case 'Black Widow':
+        console.log("one shot one kill !");
+        break;
+    case 'Iron Man':
+        console.log("i am IRON MAN...!");
+        break;
+            
+    case 'thor':
+        console.log("that is my Hammer!");
+        break;
+
+    default:
+        console.log(`Enter vaild Avenger! ${avenger} isn't an Avenger :( `);
+}
