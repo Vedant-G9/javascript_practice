f9c38fcbfe16546b11c3a8fc8fc48f5df5ac297e
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-23 01:55:08 +0530

    Ternary_operator

---
+++ operators/ternary_oprt.js
@@ -0,0 +1,16 @@
+console.log("Ternary Operator");
+
+let age = 18;
+
+//simple if else statement
+
+if (age >= 18 ){
+    console.log("you can drive");
+} else {
+    console.log('you can not drive');
+}
+
+//ternary operator
+const age1 =19;
+
+age1 >=18 ? console.log("you can drive !") : console.log("you can not drive !");
