604e77d69cc32a23dec58dc05581cae6bb088536
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-23 02:14:58 +0530

    Looping

---
+++ looping/script.js
@@ -0,0 +1,26 @@
+console.log("looping");
+
+// While Loop
+
+// while (condotion){
+//       block of code
+// }
+console.log("While Loop");
+
+let i=1;
+
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+//For Loop
+
+// for(initialization;condition;increment){
+//    code
+// }
+console.log("For Loop");
+
+for(let i=0;i < 10;i++){
+    console.log(i);
+}
