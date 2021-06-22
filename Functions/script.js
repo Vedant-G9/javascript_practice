1c44f5b0fb85dc1675811dc8fdb4efd78c50afa1
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-23 03:01:38 +0530

    Functions and Arrow Functions

---
+++ Functions/script.js
@@ -0,0 +1,58 @@
+console.log("function");
+
+//a block of code that perform a task
+
+// Function Declartion ==> Defining a function
+function square(number) {
+    return number * number;
+}
+
+
+// Function Call ==> invoking/executing of a function
+const result = square(5);
+
+console.log(result);
+
+//Function declaration 
+function funname(params){
+    //statements
+    //has access to 'this' keyword
+}
+
+//Function Expression
+const name1 = function(prams){
+    //statements
+}
+
+//An Arrow Function 
+const name = (prams)  => {
+    console.log(`Hi, ${prams}!`);
+    //statements
+}
+name('vedu');
+
+
+//example
+
+function sayHi(name){
+    console.log(`Hi, ${name}!`);
+}
+
+sayHi('vedant');
+
+//Function return undefined
+//Function return also end the function
+
+function add(a,b){
+    return a+b;
+}
+
+const sum = add(10,15);
+console.log(sum);
+
+//Arrow Function
+
+const sqr = (number) => number*number;
+
+const result1 = sqr(10);
+console.log(result1);
