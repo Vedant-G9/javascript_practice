8c9524051c505f1d71df601f23a3a216c063590e
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-29 23:43:40 +0530

    Arrays in Details

---
+++ Arrays in Detail/script.js
@@ -0,0 +1,14 @@
+console.log("---> Arrays in Detail <---");
+
+
+//odderd collection of data---array
+
+const months = ['january','february','march','Arpil'];
+
+console.log(`type of array is ${typeof months}`);
+
+console.log(months.length); //length
+
+for(let i=0;i<months.length;i++){
+    console.log(months[i]);
+}
