c6a88564425f2ceddb9a53c84e381b250f4cbabd
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-29 23:54:46 +0530

    Array functions

--- Arrays in Detail/script.js
+++ Arrays in Detail/script.js
@@ -12,3 +12,32 @@ console.log(months.length); //length
 for(let i=0;i<months.length;i++){
     console.log(months[i]);
 }
+
+//Arrays function
+
+//Array push
+
+const names = ['vedu','nishit','akash','mori'];
+
+console.log(names.push('vedant'));//------return value is the length of the array+1
+
+console.log(names);
+
+//Array pop
+
+console.log(names.pop());//---------return the deleted value
+
+console.log(names);
+
+
+//Array shift
+
+names.shift();//---------------delete the first value if the array
+
+console.log(names);
+
+//Array Unshift
+
+names.unshift('vedu');//----------adds a new value to the start of an array
+
+console.log(names);
